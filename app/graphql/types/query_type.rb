fc998c7ca75fd27e6b1df2733341a684ab95f1cd
Seungjae Lee <saystone@gmail.com>
2018-04-19 02:19:22 +0900

    user, search query 구현
    - 인스타그램 API의 /users/user-id, /users/search를 커버합니다.
    - 어떤 GraphQL query가 어떤 인스타그램 API를 커버하는지는 문서로 따로 정리할 예정입니다.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,27 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
+  # GET/users/self Get information about the owner of the access token.
+  # GET/users/self/media/recent Get the most recent media of the user.
+  # GET/users/self/media/liked Get the recent media liked by the user.
 
-  field :users, !types[Types::UserType] do
+  # GET/users/user-id Get information about a user.
+  # GET/users/user-id/media/recent Get the most recent media of a user.
+  field :user do
+    type Types::UserType
+    argument :id, types.ID
+    description "Find a user by id"
     resolve -> (obj, args, ctx) {
-      User.all
+      User.find(args["id"])
+    }
+  end
+
+  # GET/users/search Search for a user by name.
+  field :searchUser do
+    type types[Types::UserType]
+    argument :q, types.String
+    description "Search a user by query"
+    resolve -> (obj, args, ctx) {
+      User.where("full_name LIKE ?", "%#{args["q"]}%")
     }
   end
 end
