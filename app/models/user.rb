4320c11b4061c698ea961f3fbc5a8caf164307e6
Seungjae Lee <saystone@gmail.com>
2018-04-18 16:52:16 +0900

    Relationship 모델 수정
    - User 모델과 follower, following의 상호참조를 명확하게 하도록 수정합니다.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :relationships
+  has_many :followings, foreign_key: "following_id", class_name: "Relationship"
+  has_many :followers, foreign_key: "follower_id", class_name: "Relationship"
   has_many :media
 
   validates :username, presence: true, uniqueness: true
