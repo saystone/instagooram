d0039c4a75b942242de8cbfce6a9fc25e325435f
Seungjae Lee <saystone@gmail.com>
2018-04-18 17:52:45 +0900

    users graphql query 생성
    - 데이터가 제대로 반환되는지 테스트하기 위해 간단하게 구현해봅니다.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,9 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
 
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
+  field :users, !types[Types::UserType] do
+    resolve -> (obj, args, ctx) {
+      User.all
     }
   end
 end
