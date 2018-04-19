c86acc7fadc367f9c2b8138fe09ad5a32ce2d6de
Seungjae Lee <saystone@gmail.com>
2018-04-20 01:00:06 +0900

    user, search query의 인자를 필수로 변경

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,12 +3,15 @@ Types::QueryType = GraphQL::ObjectType.define do
   # GET/users/self Get information about the owner of the access token.
   # GET/users/self/media/recent Get the most recent media of the user.
   # GET/users/self/media/liked Get the recent media liked by the user.
+  # GET/users/self/follows Get the list of users this user follows.
+  # GET/users/self/followed-by Get the list of users this user is followed by.
 
   # GET/users/user-id Get information about a user.
   # GET/users/user-id/media/recent Get the most recent media of a user.
+
   field :user do
     type Types::UserType
-    argument :id, types.ID
+    argument :id, !types.ID
     description 'Find a user by id'
     resolve lambda { |_user, args, _ctx|
       User.find(args['id'])
@@ -16,9 +19,9 @@ Types::QueryType = GraphQL::ObjectType.define do
   end
 
   # GET/users/search Search for a user by name.
-  field :searchUser do
+  field :search do
     type types[Types::UserType]
-    argument :q, types.String
+    argument :q, !types.String
     description 'Search a user by query'
     resolve lambda { |_user, args, _ctx|
       User.where('full_name LIKE ?', "%#{args['q']}%")
