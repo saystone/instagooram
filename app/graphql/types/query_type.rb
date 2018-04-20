84b3d4085c98514e8b11ec2190f4c9578022dd47
Seungjae Lee <saystone@gmail.com>
2018-04-20 12:52:41 +0900

    user query 수정
    - id 인자를 optional로 변경하고 인자가 없는 경우 현재 context의 current_user로 간주합니다.
    - 이로써 */self/* 관련 인스타그램 API까지 커버가 됩니다.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,20 +1,25 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
-  # GET/users/self Get information about the owner of the access token.
-  # GET/users/self/media/recent Get the most recent media of the user.
-  # GET/users/self/media/liked Get the recent media liked by the user.
-  # GET/users/self/follows Get the list of users this user follows.
-  # GET/users/self/followed-by Get the list of users this user is followed by.
 
   # GET/users/user-id Get information about a user.
   # GET/users/user-id/media/recent Get the most recent media of a user.
-
+  # GET/users/self/media/liked Get the recent media liked by the user.
+  # GET/users/self Get information about the owner of the access token.
+  # GET/users/self/media/recent Get the most recent media of the user.
+  # GET/users/self/follows Get the list of users this user follows.
+  # GET/users/self/followed-by Get the list of users this user is followed by.
   field :user do
     type Types::UserType
-    argument :id, !types.ID
-    description 'Find a user by id'
-    resolve lambda { |_user, args, _ctx|
-      User.find(args['id'])
+    argument :id, types.ID
+    description 'Find a user by id or current context'
+    resolve lambda { |_user, args, ctx|
+      id = if args['id'].present?
+             args['id']
+           else
+             ctx[:current_user].id
+           end
+
+      User.find(id)
     }
   end
 
@@ -29,7 +34,7 @@ Types::QueryType = GraphQL::ObjectType.define do
   end
 
   # GET/media/media-id Get information about a media object.
-  # GET/media/shortcode/shortcodeGet information about a media object.
+  # GET/media/shortcode/shortcode Get information about a media object.
   field :media do
     type Types::MediaType
     argument :id, types.ID
